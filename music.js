eda44a50b43a8b924f6981ce7e227a0c775e2249
CK151 <andersonoki15@gmail.com>
2023-01-02 20:26:25 +0300

    init

---
+++ music.js
@@ -0,0 +1,37 @@
+let trackList = [
+    {
+        name: "Monisa",
+        path: "./music/ lisa.mp3",
+        img: "./images/img1.jpeg",
+        singer: "LilWayne",
+    },
+
+    {
+        name: "Mask Off",
+        path: "./music/ Off.mp3",
+        img: "./images/img2.jpeg",
+        singer: "Future",
+    },
+
+
+    {
+        name: "Devil",
+        path: "./music/devil.mp3",
+        img: "./images/img3.jpeg",
+        singer: "Devile",
+    },
+
+    {
+        name: "Famous",
+        path: "./music/Famous.mp3",
+        img: "./images/img4.jpeg",
+        singer: "LilWayne",
+    },
+
+    {
+        name: "Memories",
+        path: "./music/memories.mp3",
+        img: "./images/img2.jpeg",
+        singer: "Aiden34",
+    }
+]
